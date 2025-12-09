b132af0b4a00c75110fd7863861c356e94086a25
Keith Gardner <wkeithgardner@gmail.com>
2025-12-08 20:23:07 -0500

    Reorganized JS

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -36,60 +36,59 @@
 			$(targetClass).show();
 		});
 
+		/*
+		 * Generate AI Summaries Button
+		 *
+		 * Handle the generate summaries button click.
+		 */
+		const generateButton = $('#asc-ais-generate-button');
+		const generateStatus = $('#asc-ais-generate-status');
+		const excerptField = $('#asc_ais_excerpt');
+		const summaryField = $('#asc_ais_summary');
 
-	/*
-	 * Generate AI Summaries Button
-	 *
-	 * Handle the generate summaries button click.
-	 */
-	const generateButton = $('#asc-ais-generate-button');
-	const generateStatus = $('#asc-ais-generate-status');
-	const excerptField = $('#asc_ais_excerpt');
-	const summaryField = $('#asc_ais_summary');
+		if (generateButton.length) {
+			generateButton.on('click', function () {
+				const postId = $(this).data('post-id');
+				if (!postId) {
+					return;
+				}
 
-	if (generateButton.length) {
-		generateButton.on('click', function () {
-			const postId = $(this).data('post-id');
-			if (!postId) {
-				return;
-			}
+				// Disable button and show loading
+				generateButton.prop('disabled', true);
+				generateStatus.html('<span style="color: #2271b1;">' + ascAISummaries.i18n.generating + '</span>');
 
-			// Disable button and show loading
-			generateButton.prop('disabled', true);
-			generateStatus.html('<span style="color: #2271b1;">' + ascAISummaries.i18n.generating + '</span>');
-
-			// Make AJAX request
-			$.ajax({
-				url: ascAISummaries.ajaxUrl,
-				type: 'POST',
-				data: {
-					action: 'asc_ais_generate_summaries',
-					nonce: ascAISummaries.nonce,
-					post_id: postId,
-				},
-				success: function (response) {
-					if (response.success) {
-						if (response.data.excerpt) {
-							excerptField.val(response.data.excerpt);
+				// Make AJAX request
+				$.ajax({
+					url: ascAISummaries.ajaxUrl,
+					type: 'POST',
+					data: {
+						action: 'asc_ais_generate_summaries',
+						nonce: ascAISummaries.nonce,
+						post_id: postId,
+					},
+					success: function (response) {
+						if (response.success) {
+							if (response.data.excerpt) {
+								excerptField.val(response.data.excerpt);
+							}
+							if (response.data.summary) {
+								summaryField.val(response.data.summary);
+							}
+							generateStatus.html('<span style="color: #00a32a;">' + ascAISummaries.i18n.success + '</span>');
+							setTimeout(function () {
+								generateStatus.html('');
+							}, 3000);
+						} else {
+							generateStatus.html('<span style="color: #d63638;">' + (response.data.message || ascAISummaries.i18n.error) + '</span>');
 						}
-						if (response.data.summary) {
-							summaryField.val(response.data.summary);
-						}
-						generateStatus.html('<span style="color: #00a32a;">' + ascAISummaries.i18n.success + '</span>');
-						setTimeout(function () {
-							generateStatus.html('');
-						}, 3000);
-					} else {
-						generateStatus.html('<span style="color: #d63638;">' + (response.data.message || ascAISummaries.i18n.error) + '</span>');
-					}
-					generateButton.prop('disabled', false);
-				},
-				error: function () {
-					generateStatus.html('<span style="color: #d63638;">' + ascAISummaries.i18n.error + '</span>');
-					generateButton.prop('disabled', false);
-				},
+						generateButton.prop('disabled', false);
+					},
+					error: function () {
+						generateStatus.html('<span style="color: #d63638;">' + ascAISummaries.i18n.error + '</span>');
+						generateButton.prop('disabled', false);
+					},
+				});
 			});
-		});
-	}
-});
+		}
+	});
 })(jQuery);
